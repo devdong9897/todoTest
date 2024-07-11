ed74a9f9b08ff2cbcd103472946b41232073b614
dev.dong9897 <dve.dong9897@gmail.com>
2024-07-11 13:12:54 +0900

    삭제기능 추가

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 function addTodo() {
   if (window.event.keyCode === 13) {
     const input = document.getElementById("input-text");
-    const addBtn = document.getElementById("add-btn");
     const newLi = document.createElement("li");
+    const addBtn = document.getElementById("add-btn");
     const newSpan = document.createElement("span");
     const deleteBtn = document.createElement("button");
     const todoList = document.getElementById("todo-list");
 
     newSpan.textContent = input.value;
     todoList.appendChild(newLi);
+    deleteBtn.textContent = "X";
     newLi.appendChild(deleteBtn);
     newLi.appendChild(newSpan);
     console.log(todoList);
   }
 }
+
+const deleteAll = () => {
+  const liList = document.querySelectorAll("li");
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].remove()
+  }
+};
